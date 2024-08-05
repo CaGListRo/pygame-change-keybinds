849784e5ff238b0fd13e2c0365c810953da5dd80
CaGListRo <paco.nyns@gmail.com>
2024-08-05 17:39:23 +0200

    WIP: Implemented the options menu. Designed the background for it and displayed the keys, their use and click rects.

--- main.py
+++ main.py
@@ -11,7 +11,12 @@ class KeyBindTest:
         self.speed: int = 10
         self.clock = pg.time.Clock()
         self.fps: int = 60
+        self.state: str = "play"
         self.display = pg.display.set_mode((800, 600))
+
+        # options stuff
+        self.texts: list[str] = ["Steer up", "Steer down", "Steer left", "Steer right"]
+        self.collision_rects: list[pg.Rect] = [pg.Rect(350, 200 + i * 40, 100, 35) for i in range(4)]
         
         # player settings
         self.player: pg.Surface = pg.Surface((50, 50))
@@ -19,8 +24,18 @@ class KeyBindTest:
         self.pos = pg.Vector2((375, 275))
 
         # button stuff
-        self.font = pg.font.SysFont("comicsans", 32)
-        
+        self.font = pg.font.SysFont("comicsans", 23)
+        self.options_button = Button(text="OPTIONS", font=self.font, mid_pos=(80, 50), size=(150, 80))
+        self.back_button = Button(text="BACK", font=self.font, mid_pos=(720, 50), size=(150, 80))
+
+        # funstuff for the options background
+        self.options_background = pg.Surface((800, 600))
+        color_value_change: float = 255 / 600
+        for i in range(600):
+            blue_value = int(0 + color_value_change * i)
+            if blue_value > 255:
+                blue_value = 255
+            pg.draw.line(surface=self.options_background, color=(0, 0, blue_value), start_pos=(0, i), end_pos=(800, i), width=1)
 
         # key settings as str
         self.up: str = "K_UP"
@@ -52,9 +67,49 @@ class KeyBindTest:
                 if event.key == self.key_right:
                     self.pos.x += self.speed
 
+    def change_key(self, index) -> None:
+        print("in")
+        match index:
+            case 0:
+                pass
+            case 1:
+                pass
+            case 2:
+                pass
+            case 3:
+                pass
+
+    def check_rect_collisions(self) -> None:
+        for idx, rect in enumerate(self.collision_rects):
+            if rect.collidepoint(pg.mouse.get_pos()):
+                if pg.mouse.get_pressed()[0]:
+                    self.szaze = "change key"
+                    self.change_key(idx)
+
     def draw_window(self) -> None:
-        self.display.fill((0, 0, 0))
-        self.display.blit(self.player, self.pos)
+        if self.state == "play":
+            self.display.fill((0, 0, 0))       
+            self.display.blit(self.player, self.pos)
+            self.options_button.render(self.display)
+        elif self.state == "options":
+            self.display.blit(self.options_background, (0, 0))
+            self.back_button.render(self.display)
+            text_to_blit = self.font.render("Click in the square to change key.", True, "white")
+            self.display.blit(text_to_blit, (100, 100))
+            for idx, text in enumerate(self.texts):
+                text_to_blit = self.font.render(text, True, "white")
+                self.display.blit(text_to_blit, (200, 200 + idx * 40))
+            text_to_blit = self.font.render(self.up.strip("K_"), True, "white")
+            self.display.blit(text_to_blit, (360, 200))
+            text_to_blit = self.font.render(self.down.strip("K_"), True, "white")
+            self.display.blit(text_to_blit, (360, 240))
+            text_to_blit = self.font.render(self.left.strip("K_"), True, "white")
+            self.display.blit(text_to_blit, (360, 280))
+            text_to_blit = self.font.render(self.right.strip("K_"), True, "white")
+            self.display.blit(text_to_blit, (360, 320))
+            for rect in self.collision_rects:
+                pg.draw.rect(self.display, "white", rect, width=1)
+
 
         pg.display.update()
 
@@ -64,6 +119,13 @@ class KeyBindTest:
             self.clock.tick(self.fps)
             self.handle_events()
             self.draw_window()
+            if self.state == "play":
+                if self.options_button.check_collision():
+                    self.state = "options"
+            elif self.state == "options":
+                self.check_rect_collisions()
+                if self.back_button.check_collision():
+                    self.state = "play"
 
 
 if __name__ == "__main__":
